9b1df0c29e3c573df28b3145902b9913ea51120f
mambar7 <malibongwemambar@gmail.com>
2020-07-14 11:04:44 +0200

    first commit

---
+++ Mamba201505019.java
@@ -0,0 +1,11 @@
+public class Mamba201505019{
+	public void main (String args[]){
+		int  x = 10;
+		int y = 20;
+		int total = x*y;
+
+		System.out.print("The product of "+x+" * "+y+" = "+z);}	
+		
+	}
+
+}
